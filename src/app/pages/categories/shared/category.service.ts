d1d94ad7fde7270786737554de33fc20e200f8c3
Vitor Alves <vitoralve81@gmail.com>
2022-01-27 19:50:45 -0300

    category serviços crud

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -11,7 +11,68 @@ import {Category} from './category.model'
 })
 export class CategoryService {
 
-  private apiPath: string ='api/categories';
+  private apiPath: string ="api/categories";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAll():Observable<Category[]>{
+    return this.http.get(this.apiPath).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategories)
+    )
+  }
+
+  getById(id: number):Observable<Category>{
+
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.get(url).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  create(category: Category): Observable<Category>{
+    return this.http.post(this.apiPath, category).pipe(
+      catchError(this.handleError),
+      map(this.jsonDataToCategory)
+    )
+  }
+
+  update(category: Category): Observable<Category>{
+    const url = `${this.apiPath}/${category.id}`;
+
+    return this.http.put(url, category).pipe(
+      map(()=> category)
+    )
+  }
+
+  delete(id: number):Observable<Category>{
+    const url = `${this.apiPath}/${id}`;
+
+    return this.http.delete(url).pipe(
+      catchError(this.handleError)
+    )
+  }
+
+
+  //Metodos Privados 
+
+  private jsonDataToCategories(jsonData: any[]): Category[]{
+    const categories: Category[] = [];
+    jsonData.forEach(element => categories.push(element as Category));
+    return categories;
+  }
+
+    
+  private jsonDataToCategory(jsonData: any): Category{
+    return jsonData as Category;
+  }
+  
+  private handleError(error: any): Observable<any>{
+    console.log("Erro na Requisição =", error)
+    return throwError(error);
+  }
+
+  
 }
